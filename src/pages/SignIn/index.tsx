0e3c992897160168324da0f3e4bfdad8545e963f
Vanessa <vanessaoliveira2706@gmail.com>
2020-09-04 14:40:04 -0300

    sign in page

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,9 +1,26 @@
 import React from 'react';
+import { Image } from 'react-native';
 
-import { Container } from './styles';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+import { Container, Title } from './styles';
+
+import logoImg from '../../assets/logo.png';
 
 const SignIn: React.FC = () => {
-  return <Container />;
+  return (
+    <Container>
+      <Image source={logoImg} />
+
+      <Title>Faça seu logon</Title>
+
+      <Input name="email" icon="mail" placeholder="E-mail" />
+      <Input name="password" icon="lock" placeholder="Senha" />
+
+      <Button onPress={() => {}}>Entrar</Button>
+    </Container>
+  );
 };
 
 export default SignIn;
